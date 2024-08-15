f19a0107fe97d7a3491946a07112336e5b66dac4
LucasBardella <lucasss.bardella.f@gmail.com>
2024-08-15 09:07:11 -0300

    react 15/08

--- etec-filmes/src/App.js
+++ etec-filmes/src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
+import Container from "./components/Container";
 
 function App() {
   return (
     <>
       <Header/>
-      <Banner/>
+      <Banner image="home" />
+      <Container>
       <h1>ETEC MCM</h1>
       <p>Teste de react</p>
+      </Container>
       <Footer/>
     </>
   );
